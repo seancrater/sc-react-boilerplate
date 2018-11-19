fdf87994281061918a71cb955d4716f9a8fe7144
seancrater <sean@seancrater.com>
2018-11-19 09:41:21 -0500

    Renaming babel config

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        "emotion",
+        "react-hot-loader/babel"
+    ],
+    presets: [
+        "@babel/preset-react",
+        "airbnb"
+    ]
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => <div>Hello World!</div>;
+const App = () => <div>Hello world!</div>;
 
 export default hot(module)(App)
